3bb18536661a46d2cfb513fdce1442f6dd249a33
Oshin Acharya <oshin.acharya0664@gmail.com>
2024-11-26 22:57:45 -0600

    Code updated

--- src/utils/Index.java
+++ src/utils/Index.java
@@ -1,4 +1,28 @@
 package utils;
 
+import static utils.Methods.maxOfThree;
+import static utils.Methods.maxOfTwo;
+
+import java.util.Scanner;
+
 public class Index {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int num1 = scanner.nextInt();
+
+        System.out.print("Enter second number: ");
+        int num2 = scanner.nextInt();
+
+        System.out.println("The maximum is: " + maxOfTwo(num1, num2));
+
+        System.out.print("Enter three numbers: ");
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+
+        System.out.println("The maximum is: " + maxOfThree(a, b, c));
+    }
 }
+
--- src/utils/Methods.java
+++ src/utils/Methods.java
@@ -1,4 +1,16 @@
 package utils;
 
 public class Methods {
+    public static int maxOfTwo(int a, int b) {
+        return Math.max(a, b);
+    }
+    public static int maxOfThree(int a, int b, int c) {
+        if (a > b && a > c) {
+            return a;
+        } else if (b > c) {
+            return b;
+        } else {
+            return c;
+        }
+    }
 }
